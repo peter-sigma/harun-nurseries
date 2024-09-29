09fe78622448ae68305f03fbbeb4d4546ec61b2a
p_sigma <petergicheha39@gmail.com>
2024-09-29 08:40:41 +0300

    Added the homepage ui

---
+++ nurseries/core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', index, name='index'),
+    # path('about', about, name='about'),
+]
